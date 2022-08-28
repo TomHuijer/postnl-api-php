11524fab6c91878d31242ea5e6f0b3a2f55ee093
Tom Huijer <t.huijer@tiscali.nl>
2022-08-28 14:59:55 +0200

    php 8.2 support

--- src/HttpClient/BaseHttpClient.php
+++ src/HttpClient/BaseHttpClient.php
@@ -182,11 +182,9 @@ abstract class BaseHttpClient
      *
      * @return static
      */
-    public function setLogger(LoggerInterface $logger)
+    public function setLogger(LoggerInterface $logger): void
     {
         $this->logger = $logger;
-
-        return $this;
     }
 
     /**
